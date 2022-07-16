f780339b0b2de4c91ecbf38f7fd84285a54089f4
HenricoW <henrico.ashley.White@gmail.com>
2022-07-16 20:34:13 +0200

    feat: app wide context setup & route linking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,49 @@
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { createContext, useState } from "react";
+import type { Dispatch, SetStateAction } from "react";
 import "../index.css";
 
+interface ContextType {
+  userAcc: string;
+  setUserAcc: Dispatch<SetStateAction<string>>;
+}
+
+const initContext: ContextType = {
+  userAcc: "",
+  setUserAcc: (acc: SetStateAction<string>) => {},
+};
+
+export const AppContext = createContext<ContextType>(initContext);
+
 function App({ Component, pageProps }: AppProps) {
+  const [userAcc, setUserAcc] = useState("");
+
+  const router = useRouter();
+  const onLink = () => {
+    router.push("/linkAccount");
+  };
+
   return (
     <div className="app">
-      <div className="circle"></div>
-      <h2>Mobili-C</h2>
-      <h4>Track your Carbon emissions from driving</h4>
+      <AppContext.Provider value={{ userAcc, setUserAcc }}>
+        <div className="circle"></div>
+        <h2>Mobili-C</h2>
+        <h4>Track your Carbon emissions from driving</h4>
 
-      <Component {...pageProps} />
+        {userAcc && (
+          <>
+            <h4>Linked Account:</h4>
+            <p>{userAcc}</p>
+          </>
+        )}
+
+        <button className="config-btn" onClick={onLink}>
+          Link Account
+        </button>
+
+        <Component {...pageProps} />
+      </AppContext.Provider>
     </div>
   );
 }
