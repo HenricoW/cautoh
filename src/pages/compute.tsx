60873084b511f498c491d7ffb7611adf6eac9986
HenricoW <henrico.ashley.White@gmail.com>
2022-07-17 16:52:31 +0200

    feat: save data to back-end, minor UI changes

--- src/pages/compute.tsx
+++ src/pages/compute.tsx
@@ -1,24 +1,24 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Chart from "../components/Chart";
 import Controlls from "../components/Controlls";
 import CurrentVehicle from "../components/CurrentVehicle";
 import Distances from "../components/Distances";
 import { getDist, getTestGraphData, graphData, processData } from "../utils/helpers";
 import { ConfigData, LocationPoint, SpeedDataType } from "../utils/types";
+import { AppContext } from "./_app";
 
 const Compute = () => {
   const [isListening, setIsListening] = useState(false);
   const [locHist, setLocHist] = useState<LocationPoint[]>([]);
-  const [dataCount, setDataCount] = useState(0);
   const [watchId, setWatchId] = useState<any>();
   const [showHist, setShowHist] = useState(false);
   const [speedData, setSpeedData] = useState<SpeedDataType[]>([]);
   const [distance, setDistance] = useState(0);
   const [emissResult, setemissResult] = useState(0);
   const [currVehicle, setCurrVehicle] = useState<ConfigData | null>(null);
+  const [respMssg, setRespMssg] = useState("");
 
-  const router = useRouter();
+  const { userAcc } = useContext(AppContext);
 
   const listenLoc = () => {
     setShowHist(false);
@@ -36,13 +36,13 @@ const Compute = () => {
           };
 
           setLocHist((hist) => [...hist, locPt]);
-          setDataCount((c) => ++c);
         },
         (err) => console.log(err),
         { enableHighAccuracy: true }
       );
 
       setWatchId(wid);
+      setRespMssg("");
     } else {
       console.log("geoloc NOT supported");
     }
@@ -75,25 +75,52 @@ const Compute = () => {
 
     const emm = (currVehicle.co2pm * distance) / 1609;
     setemissResult(emm);
+
+    const dataToSave = {
+      accID: userAcc,
+      vehData: {
+        modelConfig: currVehicle.modelConfig,
+        engineConfig: currVehicle.engineConfig,
+        fuel: currVehicle.fuel,
+        co2pm: currVehicle.co2pm,
+      },
+      speedData,
+    };
+
+    fetch("api/pushData", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dataToSave),
+    })
+      .then((resp) => resp.json())
+      .then((rdata) => {
+        if (rdata.error) {
+          setRespMssg("Error saving data");
+          console.log(rdata.message);
+        } else {
+          setRespMssg("Data Saved!");
+        }
+      })
+      .catch((err) => {
+        setRespMssg("Error saving data");
+        console.log(err);
+      });
   };
 
   return (
     <>
       <CurrentVehicle currVehicle={currVehicle} />
-      <button className="config-btn" onClick={() => router.push("/")}>
-        Go Back
-      </button>
       <Controlls listenLoc={listenLoc} stopListenLoc={stopListenLoc} />
       <Distances distance={distance} />
       {showHist && <Chart speedData={speedData} />}
 
-      <button className="config-btn" onClick={onGetEmission}>
-        Calculate
+      <button className={"config-btn" + (userAcc ? "" : " disabled")} disabled={!userAcc} onClick={onGetEmission}>
+        {userAcc ? "Calculate" : "Connect to Calculate"}
       </button>
 
       <div className="result">
         <h2>{emissResult.toFixed(2)} grams of CO2</h2>
-        <p>Within 15% level of accuracy</p>
+        <p>{respMssg}</p>
       </div>
     </>
   );
